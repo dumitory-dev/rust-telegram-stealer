e1b93b6f5d43fcaf56503fb04b857856f6c8da18
Dumitory <blackmesa2018@gmail.com>
2022-06-05 07:57:48 -0700

    feat: refactor net module

--- src/utils/net.rs
+++ src/utils/net.rs
@@ -1,126 +1,120 @@
-use curl::{
-    easy::{Easy, Form},
-    Error,
-};
+use crate::utils::types::Result;
+use curl::easy::{Easy, Form};
+use regex::Regex;
 
-fn wrap_curl_err<T>(curl_result: Result<T, Error>) -> Result<T, String> {
-    match curl_result {
-        Ok(result) => {
-            return Ok(result);
-        }
-        Err(err) => {
-            return Err(format!("Error sending request! {}", err.description()));
-        }
-    }
+struct CurlPostRequestSender {
+    easy_curl: Easy,
 }
 
-pub mod anonfile {
-    use super::send_post_req;
-    use curl::easy::Form;
-    use regex::Regex;
-    use std::str;
+impl CurlPostRequestSender {
+    const STATUS_OK: u32 = 200;
 
-    // Example response:
-    // {"status":true,"data":{"file":{"url":{"full":"https://anonfiles.com/50I2v8R5xa/Cargo_lock",
-    // "short":"https://anonfiles.com/50I2v8R5xa"},
-    // "metadata":{"id":"50I2v8R5xa","name":"Cargo.lock","size":{"bytes":17775,"readable":"17.78 KB"}}}}}
-    //
-    // so we can get only short link
-    fn get_load_link_fron_resp(data: Vec<u8>) -> Result<String, String> {
-        const SHORT_LINK: u8 = 2;
-        let str_resp = match str::from_utf8(&data) {
-            Ok(str_resp) => str_resp,
-            Err(e) => {
-                return Err(format!(
-                    "Anonfile: Invalid UTF-8 sequence from response: {}",
-                    e
-                ))
-            }
-        };
-
-        let regex = Regex::new(r#"("short":")(\S+)("},"metadata")"#).unwrap();
-
-        if let Some(captures) = regex.captures(str_resp) {
-            if let Some(capture) = captures.get(SHORT_LINK.into()) {
-                return Ok(capture.as_str().to_string());
-            }
-        }
-
-        Err(format!("Anonfile: Failed to parse json response!"))
+    pub fn new(url: String) -> Result<Self> {
+        let mut easy_curl = Easy::new();
+        easy_curl.url(url.as_str())?;
+        easy_curl.post(true)?;
+        return Ok(Self { easy_curl });
     }
 
-    pub fn load_file(path_to_file: impl AsRef<std::path::Path>) -> Result<String, String> {
-        let url = "https://api.anonfiles.com/upload";
-        let mut form_data = Form::new();
-
-        if let Err(err) = form_data.part("file").file(&path_to_file).add() {
-            return Err(format!(
-                "Anonfile: Error uploading file! {}",
-                err.description()
-            ));
-        }
-
-        let resp_data = send_post_req(url, Some(form_data), None)?;
-        get_load_link_fron_resp(resp_data)
+    pub fn add_file(&mut self, path_to_file: &String) -> Result<&mut Self> {
+        let mut file_form = Form::new();
+        file_form.part("file").file(path_to_file.as_str()).add()?;
+        self.add_form_data(file_form)
     }
-}
 
-pub mod telegram {
-    use crate::utils::net::send_post_req;
+    pub fn add_post_fields(&mut self, post_fields: String) -> Result<&mut Self> {
+        self.easy_curl.post_field_size(post_fields.len() as u64)?;
+        self.easy_curl.post_fields_copy(post_fields.as_bytes())?;
+        Ok(self)
+    }
 
-    pub fn send_message(bot_token: String, user_id: String, message: String) -> Result<(), String> {
-        let token = "5279761929:AAEnsQN3NyCqW5bJndsBzWOdWbqr4G3J9bQ";
+    pub fn add_form_data(&mut self, form_data: Form) -> Result<&mut Self> {
+        self.easy_curl.httppost(form_data)?;
+        Ok(self)
+    }
 
-        let request_url = format!(r#"https://api.telegram.org/bot{}/sendMessage"#, token);
+    pub fn send_request(&mut self) -> Result<Vec<u8>> {
+        let mut response_data = Vec::new();
+        {
+            let mut transfer = self.easy_curl.transfer();
+            transfer.write_function(|new_data| {
+                response_data.extend_from_slice(new_data);
+                Ok(new_data.len())
+            })?;
+            transfer.perform()?;
+        }
+        self.check_request_result()?;
+        Ok(response_data)
+    }
 
-        let post_fileds = format!(
-            r#"chat_id={}&text={}&parse_mode=Markdown&disable_web_page_preview=True"#,
-            user_id, message
-        );
-
-        send_post_req(request_url.as_str(), None, Some(post_fileds))?;
+    fn check_request_result(&mut self) -> Result<()> {
+        let resp_code = self.easy_curl.response_code().unwrap();
+        if resp_code != CurlPostRequestSender::STATUS_OK {
+            return Err(format!("Curl: Error! Response code: {}", resp_code).into());
+        }
         Ok(())
     }
 }
 
-fn send_post_req(
-    url: &str,
-    form_data: Option<Form>,
-    fields: Option<String>,
-) -> Result<Vec<u8>, String> {
-    const STATUS_OK: u32 = 200;
-    let mut easy = Easy::new();
+pub struct AnonFilesUploader {
+    path_to_file: String,
+}
 
-    wrap_curl_err(easy.url(url))?;
-    wrap_curl_err(easy.post(true))?;
+impl AnonFilesUploader {
+    const URL: &'static str = "https://anonfiles.com/api/upload";
 
-    if let Some(form_data) = form_data {
-        wrap_curl_err(easy.httppost(form_data))?;
+    const REGEX_PATTERN: &'static str = r#"("short":")(\S+)("},"metadata")"#;
+    const SHORT_LINK_INDEX: u8 = 2;
+
+    pub const fn new(path_to_file: String) -> Self {
+        Self { path_to_file }
     }
 
-    if let Some(fileds) = fields {
-        wrap_curl_err(easy.post_field_size(fileds.len() as u64))?;
-        wrap_curl_err(easy.post_fields_copy(fileds.as_bytes()))?;
+    pub fn upload(&self) -> Result<String> {
+        let response = CurlPostRequestSender::new(AnonFilesUploader::URL.to_string())?
+            .add_file(&self.path_to_file)?
+            .send_request()?;
+        AnonFilesUploader::get_file_short_link_from_response(&response)
     }
 
-    let mut response_data = Vec::new();
-    {
-        let mut transfer = easy.transfer();
-        wrap_curl_err(transfer.write_function(|new_data| {
-            response_data.extend_from_slice(new_data);
-            Ok(new_data.len())
-        }))?;
-        wrap_curl_err(transfer.perform())?;
+    fn get_file_short_link_from_response(response_data: &Vec<u8>) -> Result<String> {
+        let str_resp = std::str::from_utf8(response_data)?;
+
+        let regex = Regex::new(AnonFilesUploader::REGEX_PATTERN).unwrap();
+
+        if let Some(captures) = regex.captures(str_resp) {
+            if let Some(capture) = captures.get(AnonFilesUploader::SHORT_LINK_INDEX.into()) {
+                return Ok(capture.as_str().to_string());
+            }
+        }
+        Err("AnonFilesUploader: Cannot get short link from response".into())
+    }
+}
+
+pub struct TelegramBotSender {
+    token: String,
+    chat_id: String,
+}
+
+impl TelegramBotSender {
+    pub const fn new(token: String, chat_id: String) -> Self {
+        Self { token, chat_id }
     }
 
-    let resp_code = easy.response_code().unwrap();
+    pub fn send_message(&self, message: String) -> Result<()> {
+        let request_url = format!(r#"https://api.telegram.org/bot{}/sendMessage"#, self.token);
+        let request_fileds = self.build_request_fileds(message);
 
-    if resp_code != STATUS_OK {
-        return Err(format!(
-            "Error sending POST request! Response code: {}",
-            resp_code
-        ));
+        CurlPostRequestSender::new(request_url)?
+            .add_post_fields(request_fileds)?
+            .send_request()?;
+        Ok(())
     }
 
-    Ok(response_data)
+    fn build_request_fileds(&self, message: String) -> String {
+        format!(
+            "chat_id={}&text={}&parse_mode=Markdown&disable_web_page_preview=True",
+            self.chat_id, message
+        )
+    }
 }
